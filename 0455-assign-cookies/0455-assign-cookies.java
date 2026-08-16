2ee0d4e983524b77c2d76a53943d53521d754584
uday-abhi <kpasupul@gitam.in>
2026-08-16 12:46:51 +0530

    Time: 15 ms (59.65%), Space: 51.4 MB (75.13%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i=0,j=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                i++;j++;
+                
+            }
+            else{
+                j++;
+            }
+
+        }
+        return i;
+    }
+}
